1391717b35b1071f42eff341b04a8a5375dc0bff
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-18 15:20:54 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+*Bonus*
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+*Super Bonus*
+Riusciamo a gestire e mostrare un loader mentre le mail non sono ancora tutte pronte? :sorriso:
+
+Consigli
+Installate e usate Postman per testare l'API e verificare come vi risponde,
+in modo da individuare la sintassi corretta per recuperare i dati che vi servono :occhiolino:
+*/
+
+console.log(Vue);
